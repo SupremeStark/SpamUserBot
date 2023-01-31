af4ef197219be6043fe067ee46552b4daf7846b1
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-31 13:26:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    API_ID = 14817849 
+    API_HASH = "f72b972aadabef20c7ea193362f7c7ae"
+    SESSION = ""
+    SESSION2 = ""
+    SESSION3 = ""
+    SESSION4 = ""
+    SESSION5 = ""
+    TOKEN = ""
+    SUDO_USERS = []
