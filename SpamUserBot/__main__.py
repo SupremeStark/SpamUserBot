5615bbaa5267e3dbbe83df71c37b4b6cca5b5d30
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-01 18:43:31 +0530

    Update __main__.py

--- SpamUserBot/__main__.py
+++ SpamUserBot/__main__.py
@@ -2,7 +2,7 @@ import asyncio
 import importlib
 
 from pyrogram import idle , filters , Client 
-from SpamUserBot import LOG , UB , UB2 , SUDO_USERS
+from SpamUserBot import LOG ,SUDO_USERS
 from SpamUserBot.modules import ALL_MODULES
 
 loop = asyncio.get_event_loop()
