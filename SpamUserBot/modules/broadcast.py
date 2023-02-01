1fcec6f1694e232dc5c10892c25ecb7e4c2e069c
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-01 23:49:56 +0530

    Create broadcast.py

--- SpamUserBot/modules/broadcast.py
+++ SpamUserBot/modules/broadcast.py
@@ -3,12 +3,18 @@ from pyrogram.enums import ChatType
 from pyrogram.errors import FloodWait 
 
 async def _broadcast(client, message):
+    try:
+        message.delete()
+    except:
+        pass 
+    text =  await message.reply("`ɢᴇᴛᴛɪɴɢ ᴀʟʟ ᴄʜᴀᴛs ɪᴅ....`)
+    replied = message.reply_to_message
     chats = []
     async for dialog in client.get_dialogs():
         if dialog.chat.type in [ChatType.SUPERGROUP,ChatType.GROUP,ChatType.PRIVATE]:
             chats.append(dialog.chat.id)
-    print(chats)
-    if message.reply_to_message:
+    await text.edit("`ɢᴏᴛ {len(chats)} ᴄʜᴀᴛs. ɴᴏᴡ sᴛᴀʀᴛɪɴɢ ʙʀᴏᴀᴅᴄᴀsᴛ.......`")
+    if replied:
         x = message.reply_to_message.id
         y = message.chat.id
         sent = 0
@@ -20,19 +26,22 @@ async def _broadcast(client, message):
                 await asyncio.sleep(e.value)
             except Exception:
                 pass
-        await message.reply_text(f"**sᴜᴄᴄᴇssғᴜʟʟʏ ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴛʜᴇ ᴍᴇssᴀɢᴇ ɪɴ {sent} ᴄʜᴀᴛs.**")
-        return
-    text = message.text.split(None, 1)[1]
-    sent = 0   
-    for i in chats:
-        try:
-            m = await client.send_message(i, text=text)           
-            sent += 1
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-        except Exception:
-            pass
-    return await message.reply_text(f"**» sᴜᴄᴄᴇssғᴜʟʟʏ ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴛʜᴇ ᴍᴇssᴀɢᴇ ɪɴ {sent} ᴄʜᴀᴛs.**")
+        return await text.edit(f"**sᴜᴄᴄᴇssғᴜʟʟʏ ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴛʜᴇ ᴍᴇssᴀɢᴇ ɪɴ {sent} ᴄʜᴀᴛs.**")
+        
+    elif len(message.command) < 2:
+        return await text.edit("**ᴇxᴀᴍᴩʟᴇ :**\n/broadcast [ᴍᴇssᴀɢᴇ] ᴏʀ [ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ]")       
+    else:
+        text = message.text.split(None, 1)[1]
+        sent = 0   
+        for i in chats:
+            try:
+                m = await client.send_message(i, text=text)           
+                sent += 1
+            except FloodWait as e:
+                await asyncio.sleep(e.value)
+            except Exception:
+                pass
+        return await text.edit(f"**» sᴜᴄᴄᴇssғᴜʟʟʏ ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴛʜᴇ ᴍᴇssᴀɢᴇ ɪɴ {sent} ᴄʜᴀᴛs.**")
 
 
 custom_handler(["broadcast","bcast"],_broadcast)
