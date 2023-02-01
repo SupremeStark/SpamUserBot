51f1be5ccf26d6e85d4cff15decce57a7ab70a03
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-01 18:53:25 +0530

    Update __main__.py

--- SpamUserBot/__main__.py
+++ SpamUserBot/__main__.py
@@ -15,7 +15,7 @@ async def start_bot():
     LOG.print("[bold red]ᴄᴀɴᴄᴇʟɪɴɢ ᴀʟʟ ᴛᴀsᴋs.")
 
 
-@Client.on_message(filters.command(["start"], ".") & (filters.me | filters.user(SUDO_USERS)))
+@Client.on_message(filters.command("start", prefixes = ["."]) & (filters.me | filters.user(SUDO_USERS)))
 async def _start(_, message):
     await message.reply_text("ok working")
 
