a3887b26f5b4c8576d1674bfd9f0a8028165e039
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-01 23:15:00 +0530

    Create stats.py

---
+++ SpamUserBot/modules/stats.py
@@ -0,0 +1,36 @@
+from SpamUserBot.steve.funcs import custom_handler
+from pyrogram.enums import ChatType
+
+async def _stats(client, message):
+    try:
+        await message.delete()
+    except:
+        pass
+    text = await message.reply("`ᴘʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs......`")
+    user = 0
+    bots = 0
+    groups = 0
+    channels = 0    
+    async for dialog in client.get_dialogs():
+        if dialog.chat.type in [ChatType.BOT,ChatType.SUPERGROUP,ChatType.CHANNEL,ChatType.GROUP]:
+            pass
+        else:
+            user += 1
+        if dialog.chat.type in [ChatType.SUPERGROUP,ChatType.GROUP]:
+            groups += 1
+        elif dialog.chat.type == ChatType.CHANNEL :
+            channels += 1
+        elif dialog.chat.type == ChatType.BOT :
+            bots += 1
+    done = "💖 ʜᴇʀᴇ's ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs....\n\n"
+    done += f"⦾ ʙᴏᴛs : {bots}\n"
+    done += f"⦾ ᴜsᴇʀs : {users}\n"
+    done += f"⦾ ɢʀᴏᴜᴘs : {groups}\n"
+    done += f"⦾ ᴄʜᴀɴɴᴇʟs : {channels}\n"
+    await text.edit(done)
+
+
+custom_handler("stats",_stats)
+
+        
+    
