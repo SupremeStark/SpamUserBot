c10fd7b55287b68b3afd5f4aece569a6a6fde72f
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-01 22:34:46 +0530

    Update restart.py

--- SpamUserBot/modules/restart.py
+++ SpamUserBot/modules/restart.py
@@ -3,13 +3,16 @@ from SpamUserBot.steve.funcs import custom_handler
 
 
 async def _restart(client, message):
-    await message.delete()
+    try:
+        await message.delete()
+    except:
+        pass
     text = await message.reply("🎣 sᴛᴀʀᴛɪɴɢ ᴀ ɴᴇᴡ ɪɴsᴛᴀɴᴄᴇ ᴀɴᴅ sʜᴜᴛᴛɪɴɢ ᴅᴏᴡɴ ᴛʜɪs ᴏɴᴇ.......")    
     try:
         os.system(f"kill -9 {os.getpid()} && python3 -m TeleBot")
     except Exception as er:
         print(er)
-    await client.edit_message_text(message.chat.id,text.id,"**💝 ʀᴇsᴛᴀʀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.**)
+    await client.edit_message_text(message.chat.id,text.id,"**💝 ʀᴇsᴛᴀʀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.**")
 
 
 custom_handler("restart",_restart)
