9b8d57e42f6fe95554932baabd22906149346eca
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-02 00:53:17 +0530

    Update spam.py

--- SpamUserBot/modules/spam.py
+++ SpamUserBot/modules/spam.py
@@ -1,15 +1,26 @@
 from SpamUserBot.steve.funcs import custom_handler
 
 async def _spam(client, message):
-    mod_use = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\ɴ\ɴᴄᴏᴍᴍᴀɴᴅ:\n\n.sᴘᴀᴍ <ᴄᴏᴜɴᴛ> <ᴍᴇssᴀɢᴇ ᴛᴏ sᴘᴀᴍ>\n\n.sᴘᴀᴍ <ᴄᴏᴜɴᴛ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ>\n\ncᴏᴜɴᴛ ᴍᴜsᴛ ʙᴇ ᴀ ɪɴᴛᴇɢᴇʀ."
+    mod_use = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nᴄᴏᴍᴍᴀɴᴅ:\n\n.sᴘᴀᴍ <ᴄᴏᴜɴᴛ> <ᴍᴇssᴀɢᴇ ᴛᴏ sᴘᴀᴍ>\n\n.sᴘᴀᴍ <ᴄᴏᴜɴᴛ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ>\n\ncᴏᴜɴᴛ ᴍᴜsᴛ ʙᴇ ᴀ ɪɴᴛᴇɢᴇʀ."
     err_msg = "sᴘᴀᴍ Mᴏᴅᴜʟᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜsᴇᴅ ᴛɪʟʟ 𝟷𝟶𝟶 ᴄᴏᴜɴᴛ. Fᴏʀ ʙɪɢɢᴇʀ sᴘᴀᴍs ᴜsᴇ BɪɢSᴘᴀᴍ"
     try:
         await message.delete()
     except:
         pass
     replied = message.reply_to_message
-    if not replied or len(message.command) < 2:
+
+    if replied and len(message.command) != 2:
         await message.reply_text(mod_use)
+    elif not replied and len(message.command) != 3:
+        await message.reply_text(mod_use)
+    if replied:
+        if replied.caption:
+            text = replied.caption
+        else:
+            text = replied.caption 
+    else:
+        text = message.text.split(None,1)[1]
+    print(text)
 
 
 custom_handler("spam",_spam)
