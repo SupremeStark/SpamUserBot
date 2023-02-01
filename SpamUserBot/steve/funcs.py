b1dcd31235610564f3f7ccdb65b6c6c771a9d453
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-01 19:51:59 +0530

    Update funcs.py

--- SpamUserBot/steve/funcs.py
+++ SpamUserBot/steve/funcs.py
@@ -1,6 +1,6 @@
 from pyrogram.hanlders import MessageHandler
-from SpamUserBot import UB, UB2
+from SpamUserBot import UB, UB2 , PREFIXES
 
 def custom_handler(cmd,func):
-    UB.add_handler(MessageHandler(func, filters.command(cmd,prefixes = ["."]) & filters.user(SUDO_USERS)))
-    UB2.add_handler(MessageHandler(func, filters.command(cmd,prefixes = ["."]) & filters.user(SUDO_USERS)))
+    UB.add_handler(MessageHandler(func, filters.command(cmd,PREFIXES) & filters.user(SUDO_USERS)))
+    UB2.add_handler(MessageHandler(func, filters.command(cmd,PREFIXES) & filters.user(SUDO_USERS)))
