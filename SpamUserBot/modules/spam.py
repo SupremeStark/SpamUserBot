aa38a1f453fa41854ecda77e63dddafa6d1791c4
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-02 19:06:04 +0530

    Update spam.py

--- SpamUserBot/modules/spam.py
+++ SpamUserBot/modules/spam.py
@@ -5,19 +5,15 @@ from pyrogram.errors import FloodWait
 
 async def _spam(client, message):
     mod_use = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nᴄᴏᴍᴍᴀɴᴅ:\n\n.sᴘᴀᴍ <ᴄᴏᴜɴᴛ> <ᴍᴇssᴀɢᴇ ᴛᴏ sᴘᴀᴍ>\n\n.sᴘᴀᴍ <ᴄᴏᴜɴᴛ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ>\n\ncᴏᴜɴᴛ ᴍᴜsᴛ ʙᴇ ᴀ ɪɴᴛᴇɢᴇʀ."
-    err_msg = "sᴘᴀᴍ Mᴏᴅᴜʟᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜsᴇᴅ ᴛɪʟʟ 𝟷𝟶𝟶 ᴄᴏᴜɴᴛ. Fᴏʀ ʙɪɢɢᴇʀ sᴘᴀᴍs ᴜsᴇ BɪɢSᴘᴀᴍ"
+    err_msg = "sᴘᴀᴍ Mᴏᴅᴜʟᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜsᴇᴅ ᴛɪʟʟ 𝟷𝟶𝟶 ᴄᴏᴜɴᴛ."
     try:
         await message.delete()
     except:
         pass
     replied = message.reply_to_message
 
-    if message.text[0].isalpha() and message.text[0] in PREFIXES :
+    if (message.text[0].isalpha() and message.text[0] in PREFIXES) or len(message.command) == 1  :
         await message.reply_text(mod_use)
-
-    if len(message.command) == 1:
-        await message.reply_text(mod_use)
-
     replied = message.reply_to_message
     text = ("".join(message.text.split(maxsplit=1)[1:])).split(" ", 1)    
     if len(text) == 2:
@@ -50,7 +46,23 @@ async def _spam(client, message):
             except FloodWait as m:
                 await asyncio.sleep(m.value)  
              
-        
+    if replied and replied.video:
+        photo = replied.video.file_id 
+        if text[0].isdigit():
+            num = int(text[0]) 
+        else:
+            await message.reply_text("**𝟸ɴᴅ ᴀʀɢᴜᴍᴇɴᴛ ᴍᴜsᴛ ʙᴇ ᴀɴ ɪɴᴛᴇɢᴇʀ**")             
+        if not replied.caption:
+            txt = None
+        txt = replied.caption
+        if num > 100:
+            await message.reply_text(err_msg)
+        for i in range(num):
+            try:
+                await client.send_video(message.chat.id,photo=video, caption=txt) 
+            except FloodWait as m:
+                await asyncio.sleep(m.value)  
+                     
         
 
 custom_handler("spam",_spam)
