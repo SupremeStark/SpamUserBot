9ac376e64a65b314af63d216d084ff44b8e39b03
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-02 19:44:40 +0530

    Update spam.py

--- SpamUserBot/modules/spam.py
+++ SpamUserBot/modules/spam.py
@@ -3,6 +3,10 @@ from SpamUserBot import PREFIXES
 from SpamUserBot.steve.funcs import custom_handler
 from pyrogram.errors import FloodWait 
 
+SPAM_CHATS = []
+
+
+
 async def _spam(client, message):
     mod_use = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nᴄᴏᴍᴍᴀɴᴅ:\n\n.sᴘᴀᴍ <ᴄᴏᴜɴᴛ> <ᴍᴇssᴀɢᴇ ᴛᴏ sᴘᴀᴍ>\n\n.sᴘᴀᴍ <ᴄᴏᴜɴᴛ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ>\n\ncᴏᴜɴᴛ ᴍᴜsᴛ ʙᴇ ᴀ ɪɴᴛᴇɢᴇʀ."
     err_msg = "sᴘᴀᴍ Mᴏᴅᴜʟᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜsᴇᴅ ᴛɪʟʟ 𝟷𝟶𝟶 ᴄᴏᴜɴᴛ."
@@ -11,9 +15,10 @@ async def _spam(client, message):
     except:
         pass
     replied = message.reply_to_message
-
-    if (message.text[0].isalpha() and message.text[0] in PREFIXES) or len(message.command) == 1  :
-        await message.reply_text(mod_use)
+    chat_id = message.chat.id
+    SPAM_CHATS.appned(chat_id)
+    if len(message.command) == 1  :
+        await message.reply_text(mod_use)     
     replied = message.reply_to_message
     text = ("".join(message.text.split(maxsplit=1)[1:])).split(" ", 1)    
     if len(text) == 2:
@@ -22,9 +27,9 @@ async def _spam(client, message):
         else:
             await message.reply_text("**𝟸ɴᴅ ᴀʀɢᴜᴍᴇɴᴛ ᴍᴜsᴛ ʙᴇ ᴀɴ ɪɴᴛᴇɢᴇʀ**")
         to_spam = str(text[1])
-        if num > 100:
-            await message.reply_text(err_msg)
         for i in range(num):
+            if chat_id not in SPAM_CHATS:
+                break
             try:
                 await client.send_message(message.chat.id,to_spam)
             except FloodWait as m:
@@ -37,10 +42,10 @@ async def _spam(client, message):
             await message.reply_text("**𝟸ɴᴅ ᴀʀɢᴜᴍᴇɴᴛ ᴍᴜsᴛ ʙᴇ ᴀɴ ɪɴᴛᴇɢᴇʀ**")             
         if not replied.caption:
             txt = None
-        txt = replied.caption
-        if num > 100:
-            await message.reply_text(err_msg)
+        txt = replied.caption        
         for i in range(num):
+            if chat_id not in SPAM_CHATS:
+                break
             try:
                 await client.send_photo(message.chat.id,photo=photo, caption=txt) 
             except FloodWait as m:
@@ -55,9 +60,9 @@ async def _spam(client, message):
         if not replied.caption:
             txt = None
         txt = replied.caption
-        if num > 100:
-            await message.reply_text(err_msg)
         for i in range(num):
+            if chat_id not in SPAM_CHATS:
+                break
             try:
                 await client.send_video(message.chat.id,video=video, caption=txt) 
             except FloodWait as m:
@@ -72,9 +77,9 @@ async def _spam(client, message):
         if not replied.caption:
             txt = None
         txt = replied.caption
-        if num > 100:
-            await message.reply_text(err_msg)
         for i in range(num):
+            if chat_id not in SPAM_CHATS:
+                break
             try:
                 await client.send_document(message.chat.id,doc, caption=txt) 
             except FloodWait as m:
@@ -86,23 +91,41 @@ async def _spam(client, message):
             num = int(text[0]) 
         else:
             await message.reply_text("**𝟸ɴᴅ ᴀʀɢᴜᴍᴇɴᴛ ᴍᴜsᴛ ʙᴇ ᴀɴ ɪɴᴛᴇɢᴇʀ**")             
-        if num > 100:
-            await message.reply_text(err_msg)
         for i in range(num):
+            if chat_id not in SPAM_CHATS:
+                break
             try:
                 await client.send_sticker(message.chat.id,sticker) 
             except FloodWait as m:
                 await asyncio.sleep(m.value)  
-                             
+
+
+    if replied and replied.animation:
+        file = replied.animation.file_id
+        if text[0].isdigit():
+            num = int(text[0]) 
+        else:
+            await message.reply_text("**𝟸ɴᴅ ᴀʀɢᴜᴍᴇɴᴛ ᴍᴜsᴛ ʙᴇ ᴀɴ ɪɴᴛᴇɢᴇʀ**")             
+        if not replied.caption:
+            txt = None
+        txt = replied.caption
+        for i in range(num):
+            if chat_id not in SPAM_CHATS:
+                break
+            try:
+                await client.send_animation(message.chat.id, animation = file, caption=txt) 
+            except FloodWait as m:
+                await asyncio.sleep(m.value)  
+                                                          
     if replied and replied.text:
         msg = replied.text 
         if text[0].isdigit():
             num = int(text[0]) 
         else:
-            await message.reply_text("**𝟸ɴᴅ ᴀʀɢᴜᴍᴇɴᴛ ᴍᴜsᴛ ʙᴇ ᴀɴ ɪɴᴛᴇɢᴇʀ**")             
-        if num > 100:
-            await message.reply_text(err_msg)
+            await message.reply_text("**𝟸ɴᴅ ᴀʀɢᴜᴍᴇɴᴛ ᴍᴜsᴛ ʙᴇ ᴀɴ ɪɴᴛᴇɢᴇʀ**")                     
         for i in range(num):
+            if chat_id not in SPAM_CHATS:
+                break
             try:
                 await client.send_message(message.chat.id,msg) 
             except FloodWait as m:
