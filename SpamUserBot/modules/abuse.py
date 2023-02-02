e8dcc63b4a609a40129ec03630c1bae68554c4eb
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-02 22:49:11 +0530

    Update abuse.py

--- SpamUserBot/modules/abuse.py
+++ SpamUserBot/modules/abuse.py
@@ -7,8 +7,9 @@ async def _abuse(client, message):
         await message.delete()
     except:
         pass
+    print(await extract_user_id(message))
     if len(message.command) == 1:
         return await message.reply_text("**Mᴏᴅᴜʟᴇ ɴᴀᴍᴇ = ᴀʙᴜsᴇ**\n\nCᴏᴍᴍᴀɴᴅ:\n\n .ɢᴀʟɪ <ᴜsᴇʀ ʜᴀɴᴅʟᴇʀ>\n\nɪᴛ ᴡɪʟʟ ᴄᴏɴᴛɪɴᴜᴏᴜsʟʏ ᴀʙᴜsᴇ ᴜɴᴛɪʟ ʏᴏᴜ ʀᴇsᴛᴀʀᴛ!!.")
-    print(await extract_user_id(message))
+    
 
 custom_handler("abuse",_abuse)
