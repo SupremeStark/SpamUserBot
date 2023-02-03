7818e16889e67075fe45630c96f5c66e8ca4883b
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-03 15:57:34 +0530

    Update raid.py

--- SpamUserBot/modules/raid.py
+++ SpamUserBot/modules/raid.py
@@ -9,6 +9,7 @@ async def _raid(client, message):
     if len(message.command) == 1:
         await message.reply_text("𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗥𝗮𝗶𝗱\n\nᴄᴏᴍᴍᴀɴᴅ:\n\n.ʀᴀɪᴅ <ᴜsᴇʀ ʜᴀɴᴅʟᴇʀ>\n\nᴄᴏᴜɴᴛ ᴍᴜsᴛ ʙᴇ ᴀ ɪɴᴛᴇɢᴇʀ.")
     text = message.text.split()
+    text.pop(0)
     print(text)
    # if len(text) == 2:
    #     count = int(text[1]) if text[1].isdigit() else await message.reply("𝟸ɴᴅ ᴀʀɢᴜᴍᴇɴᴛ ᴍᴜsᴛ ʙᴇ ᴀɴ ɪɴᴛᴇɢᴇʀ.")
