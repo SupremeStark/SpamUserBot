39e49226a366c752c975d82568df03d71b376e41
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-03 06:45:12 +0530

    Update raid.py

--- SpamUserBot/modules/raid.py
+++ SpamUserBot/modules/raid.py
@@ -1,5 +1,5 @@
 from SpamUserBot.modules.cancel import SPAM_CHATS
-
+from SpamUserBot.steve.funcs import custom_handler
 
 async def _raid(client, message):
     try:
@@ -9,3 +9,6 @@ async def _raid(client, message):
     if len(message.command) == 1:
         await message.reply_text("𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗥𝗮𝗶𝗱\n\nᴄᴏᴍᴍᴀɴᴅ:\n\n.ʀᴀɪᴅ <ᴜsᴇʀ ʜᴀɴᴅʟᴇʀ>\n\nᴄᴏᴜɴᴛ ᴍᴜsᴛ ʙᴇ ᴀ ɪɴᴛᴇɢᴇʀ.")
     print(message.command)    
+
+
+custom_handler("raid",_raid)
