c3c60bae363a92ceaf1ab72d64b79e481698add3
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-04 11:17:05 +0530

    Update cancel.py

--- SpamUserBot/modules/cancel.py
+++ SpamUserBot/modules/cancel.py
@@ -1,3 +1,19 @@
-
+from SpamUserBot.steve.funcs import custom_handler
 
 SPAM_CHATS = []
+
+async def cancelcmd(_, message):
+    chat_id = message.chat.id
+    if chat_id in SPAM_CHATS:
+        try :
+            SPAM_CHATS.remove(chat_id)
+        except Exception:
+            pass   
+        return await message.reply_text("sᴛᴏᴘᴘᴇᴅ ᴛʜᴇ ᴘʀᴏᴄᴇss.")     
+                                     
+    else :
+        await message.reply_text("**ᴛʜᴇʀᴇ ɪs ɴᴏ ᴘʀᴏᴄᴇss ɢᴏɪɴɢ ᴏɴ ʙᴀʙʏ.**")  
+        return       
+
+
+custom_hanlder(["cancel","stop"],cancelcmd)
