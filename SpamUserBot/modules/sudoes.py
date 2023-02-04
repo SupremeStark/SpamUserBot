fe826f342c894023ef681f1712a41cf1f931166e
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-04 08:04:26 +0530

    Create sudoes.py

---
+++ SpamUserBot/modules/sudoes.py
@@ -0,0 +1,70 @@
+import os
+import json 
+from SpamUserBot import SUDO_USERS
+from SpamUserBot.steve.funcs import custom_handler,extract_user_id
+
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "SpamUserBot/sudo.json")
+
+async def _add_sudo(client, message):
+    try:
+        await message.delete()
+    except:
+        pass
+    user_id = extract_user_id(message)
+    if not user_id:
+        return await message.reply("ʙʀᴜʜ sᴘᴇᴄɪғʏ ᴀ ᴜsᴇʀ.")
+    if user_id in SUDO_USERS:
+        return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ɪs ᴀʟʀᴇᴀᴅʏ ᴀ sᴜᴅᴏ ᴜsᴇʀ.")
+    with open(ELEVATED_USERS_FILE, "r") as infile:
+        data = json.load(infile)
+    data["sudos"].append(user_id)
+    SUDO_USERS.append(user_id)
+    with open(ELEVATED_USERS_FILE, "w") as outfile:
+        json.dump(data, outfile, indent=4)
+    return await message.reply("ᴀᴅᴅᴇᴅ ᴛʜɪs ᴜsᴇʀ ɪɴ sᴜᴅᴏ ᴜsᴇʀs.")
+
+async def _rm_sudo(client, message):
+    try:
+        await message.delete()
+    except:
+        pass
+    user_id = extract_user_id(message)
+    if not user_id:
+        return await message.reply("ʙʀᴜʜ sᴘᴇᴄɪғʏ ᴀ ᴜsᴇʀ.")
+    with open(ELEVATED_USERS_FILE, "r") as infile:
+        data = json.load(infile)
+
+    if user_id not in SUDO_USERS:
+        return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ᴀɪɴ'ᴛ ᴀ sᴜᴅᴏ ᴜsᴇʀ ᴀɴʏᴡᴀʏ ɪɢɴᴏʀᴇ ʜɪᴍ.")
+    SUDO_USERS.remove(user_id)
+    data["sudos"].remove(user_id)
+    with open(ELEVATED_USERS_FILE, "w") as outfile:
+            json.dump(data, outfile, indent=4)
+
+async def sudo_list(client, message):
+    try:
+        await message.delete()
+    except:
+        pass
+    text = "💝 sᴜᴅᴏ ᴜsᴇʀs:\n" 
+    for i in SUDO_USERS:
+        user_id = int(i)
+        try:
+            mention = (await client.get_users(user_id)).mention
+            text += f"• {mention}"
+        except Exception as e:
+            print(e)
+    return await message.reply_text(text)
+
+
+
+
+
+
+
+   
+
+
+
+  
+ 
