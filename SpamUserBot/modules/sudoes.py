49bfad9f497742999fa7ff34c4a9d21962e9f07c
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-04 08:29:44 +0530

    Update sudoes.py

--- SpamUserBot/modules/sudoes.py
+++ SpamUserBot/modules/sudoes.py
@@ -6,40 +6,35 @@ from SpamUserBot.steve.funcs import custom_handler,extract_user_id
 ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "SpamUserBot/sudo.json")
 
 async def _add_sudo(client, message):
+    user_id = await extract_user_id(message)
     try:
         await message.delete()
     except:
-        pass
-    user_id = await extract_user_id(message)
+        pass    
     if not user_id:
         return await message.reply("ʙʀᴜʜ sᴘᴇᴄɪғʏ ᴀ ᴜsᴇʀ.")
     if user_id in SUDO_USERS:
         return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ɪs ᴀʟʀᴇᴀᴅʏ ᴀ sᴜᴅᴏ ᴜsᴇʀ.")
-    with open(ELEVATED_USERS_FILE, "r") as infile:
-        data = json.load(infile)
-    data["sudos"].append(user_id)
+  #  with open(ELEVATED_USERS_FILE, "r") as infile:
+  #       data = json.load(infile)
+   # data["sudos"].append(user_id)
     SUDO_USERS.append(user_id)
-    with open(ELEVATED_USERS_FILE, "w") as outfile:
-        json.dump(data, outfile, indent=4)
+   # with open(ELEVATED_USERS_FILE, "w") as outfile:
+  #      json.dump(data, outfile, indent=4)
     return await message.reply_text("ᴀᴅᴅᴇᴅ ᴛʜɪs ᴜsᴇʀ ɪɴ sᴜᴅᴏ ᴜsᴇʀs.")
 
 async def _rm_sudo(client, message):
+    user_id = await extract_user_id(message)
     try:
         await message.delete()
     except:
-        pass
-    user_id = await extract_user_id(message)
+        pass   
     if not user_id:
         return await message.reply("ʙʀᴜʜ sᴘᴇᴄɪғʏ ᴀ ᴜsᴇʀ.")
-    with open(ELEVATED_USERS_FILE, "r") as infile:
-        data = json.load(infile)
-
+    
     if user_id not in SUDO_USERS:
         return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ᴀɪɴ'ᴛ ᴀ sᴜᴅᴏ ᴜsᴇʀ ᴀɴʏᴡᴀʏ ɪɢɴᴏʀᴇ ʜɪᴍ.")
-    SUDO_USERS.remove(user_id)
-    data["sudos"].remove(user_id)
-    with open(ELEVATED_USERS_FILE, "w") as outfile:
-            json.dump(data, outfile, indent=4)
+    SUDO_USERS.remove(user_id)    
     return await message.reply_text("ʀᴇᴍᴏᴠᴇᴅ ғʀᴏᴍ sᴜᴅᴏ ᴜsᴇʀs")
 
 async def sudo_list(client, message):
